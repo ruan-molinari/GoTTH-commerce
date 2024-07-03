c6b377337d36051a8699670054da43d7e09c32bf
ruan-molinari <ruan.molinari@proton.me>
2024-07-02 21:31:02 -0300

    Adding chi

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	err := http.ListenAndServe(":3333", r)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module GoTTH-commerce
 
 go 1.22.3
+
+require (
+	github.com/a-h/templ v0.2.731 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
